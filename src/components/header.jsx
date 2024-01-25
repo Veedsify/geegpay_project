8dd9fac9b00aeaf7604f9bb23d894e9da2940d28
dike wisdom <dikewisdom787@gmail.com>
2024-01-25 11:43:47 +0100

    header update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,40 @@
 import { useState } from "react";
-import { ChevronDownIcon, LucideMenu, LucideSearch, X } from "lucide-react";
+import {
+  ChevronDownIcon,
+  LucideMenu,
+  LucideSearch,
+  X,
+  LucideUser,
+  LucideSettings,
+  LucideLogOut,
+} from "lucide-react";
 import MyCalendar from "./sub/calender";
 import { useCalendarContext } from "../libs/calendarcontext";
 import { useSearchBarContext, useSideBarContext } from "../libs/context";
+import { Link } from "react-router-dom";
 const Header = () => {
   const [showNotifications, setShowNotifications] = useState(false);
   const [showCalendar, setCalendar] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   const { selectedDate } = useCalendarContext();
   const { active, setActive } = useSideBarContext();
   const { setSearchActive } = useSearchBarContext();
 
   const toggleNotifications = () => {
     setShowNotifications(!showNotifications);
+    setCalendar(false);
+    setShowProfile(false);
   };
   const toggleCalendar = () => {
     setCalendar(!showCalendar);
+    setShowNotifications(false);
+    setShowProfile(false);
+  };
+
+  const toggleProfile = () => {
+    setShowProfile(!showProfile);
+    setCalendar(false);
+    setShowNotifications(false);
   };
 
   const toggleSideBar = () => {
@@ -87,13 +107,16 @@ const Header = () => {
             )}
           </li>
           <li className="relative border p-1 rounded-full">
-            <div className="flex items-center gap-x-3  cursor-pointer">
+            <div
+              className="flex items-center gap-x-3  cursor-pointer"
+              onClick={toggleProfile}
+            >
               <img
                 src="/icons/profile.png"
                 alt="Profile Icon"
                 className="cursor-pointer flex-1"
               />
-              <div className="text-right flex-1">
+              <div className="text-right flex-1 select-none cursor-pointer">
                 <p className="font-[600] leading-4">Justin Bergson</p>
                 <p className="text-sm">Justin@gmail.com</p>
               </div>
@@ -101,6 +124,28 @@ const Header = () => {
                 <ChevronDownIcon />
               </div>
             </div>
+            {showProfile && (
+              <div className="absolute right-0 mt-4 w-48 bg-white rounded-md shadow-lg py-2">
+                <div className="p-2 text-sm hover:bg-[#34CAA5] cursor-pointer mb-2 hover:text-white">
+                  <Link to="" className="flex items-center gap-2">
+                    <LucideUser />
+                    Profile
+                  </Link>
+                </div>
+                <div className="p-2 text-sm hover:bg-[#34CAA5] cursor-pointer mb-2 hover:text-white">
+                  <Link to="" className="flex items-center gap-2">
+                    <LucideSettings />
+                    Settings
+                  </Link>
+                </div>
+                <div className="p-2 text-sm hover:bg-[#34CAA5] cursor-pointer hover:text-white">
+                  <Link to="" className="flex items-center gap-2">
+                    <LucideLogOut />
+                    Logout
+                  </Link>
+                </div>
+              </div>
+            )}
           </li>
         </ul>
         {/* MENU BUTTON */}
