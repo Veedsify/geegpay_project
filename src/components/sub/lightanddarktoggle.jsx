5a60e94890b1205584691271e65f23e3899ac021
dike wisdom <dikewisdom787@gmail.com>
2024-01-25 16:30:16 +0100

    updated sidebar

--- src/components/sub/lightanddarktoggle.jsx
+++ src/components/sub/lightanddarktoggle.jsx
@@ -8,12 +8,17 @@ const LightAndDarkToggle = () => {
   };
 
   return (
-    <div className="px-3 md:px-4 cursor-pointer select-none" onClick={toggleTheme}>
+    <div
+      className="px-3 md:px-4 cursor-pointer select-none mb-6"
+      onClick={toggleTheme}
+    >
       <div className="flex flex-col gap-y-3 items-center justify-center border bg-white shadow-sm p-1  rounded-full">
         <img
           src="/icons/lightmode.png"
           alt="Light Mode"
-          className={`duration-300 transition-all rounded-full border ${order ? "order-2" : ""}`}
+          className={`duration-300 transition-all rounded-full border ${
+            order ? "order-2" : ""
+          }`}
         />
         <img src="/icons/darkmode.png" alt="Light Mode" />
       </div>
